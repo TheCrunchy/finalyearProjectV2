803ea301c439b3da3d0c2a80f9ad9c22d47df02b
TheCrunchy <crunchycamsta@gmail.com>
2020-03-10 11:21:01 +0000

    Revert "Sort of better but broken"
    This reverts commit 8a798aa2a5e38276568cb740c5c784558b3c6359.

--- app/src/main/java/com/cameron/finalyearprojectv2/DateForSpinner.java
+++ app/src/main/java/com/cameron/finalyearprojectv2/DateForSpinner.java
@@ -1,23 +1,31 @@
 package com.cameron.finalyearprojectv2;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
-public class DateForSpinner{
-    private LocalDate date;
+public class DateForSpinner implements Comparable<TimeTable>{
+    private Calendar date;
 
-    public DateForSpinner(LocalDate cal){
+    public DateForSpinner(Calendar cal){
         this.date = cal;
     }
 
-    public LocalDate getDateTime(){
+    public Calendar getDateTime(){
         return date;
     }
+
+    @Override
+    public int compareTo(TimeTable o) {
+        return getDateTime().compareTo(o.getDateTime());
+    }
+
 
     //make this return the monday instead of making the calendar monday
     @Override
     public String toString() {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return date.format(dateFormatter);
+
+     //return String.valueOf(date.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY));
+
+    //return "Week Beginning " + date.getTime().getDate() +  "/" + (date.getTime().getMonth() + 1) +  "/" + (date.getTime().getYear() + 1900);
+        return date.getTime().getDate() +  "/" + (date.getTime().getMonth() + 1) +  "/" + (date.getTime().getYear() + 1900);
     }
 }
